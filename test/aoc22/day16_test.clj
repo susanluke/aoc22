06e45ec7fbc4e71a0f37f5109ed8696edec6a6db
Susan Luke <sluke000@gmail.com>
2023-01-28 12:46:50 +0000

    Update comments for tests

--- test/aoc22/day16_test.clj
+++ test/aoc22/day16_test.clj
@@ -24,63 +24,29 @@ Valve JJ has flow rate=21; tunnel leads to valve II")
   (is (= 1653 (:pressure-released (sut/pt1 input2)))))
 
 (deftest pt2
-  (is (= 1707 (:pressure-released (sut/pt2 input)))))
+  (is (= 1707 (:pressure-released (sut/pt2 input))))
+  ;; takes 5.5h!
+  ;;(is (= 2223 (:pressure-released (sut/pt2 input2))))
+  )
 
 (comment
-  ;; input takes ~10s
-  {:pressure-released 1651,
-   :actions
-   [[7 [:valve-on :CC]]
-    [8 [:travel :CC]]
-    [9 [:travel :DD]]
-    [10 [:valve-on :EE]]
-    [11 [:travel :EE]]
-    [12 [:travel :FF]]
-    [13 [:travel :GG]]
-    [14 [:valve-on :HH]]
-    [15 [:travel :HH]]
-    [16 [:travel :GG]]
-    [17 [:travel :FF]]
-    [18 [:travel :EE]]
-    [19 [:travel :DD]]
-    [20 [:travel :AA]]
-    [21 [:travel :II]]
-    [22 [:valve-on :JJ]]
-    [23 [:travel :JJ]]
-    [24 [:travel :II]]
-    [25 [:travel :AA]]
-    [26 [:valve-on :BB]]
-    [27 [:travel :BB]]
-    [28 [:travel :CC]]
-    [29 [:valve-on :DD]]
-    [30 [:travel :DD]]]}
+  ;; Pt2: input2 takes 5.5hrs
+  ;; Need to prune route choices and dismiss routes which obviously don't make sense
 
-  ;; input2 takes a few hours
-  {:pressure-released 1653,
+  "Elapsed time: 2.0527428064667E7 msecs"
+  {:pressure-released 2223,
    :actions
-   [[5 [:valve-on :HR]]
-    [6 [:travel :HR]]
-    [7 [:travel :KO]]
-    [8 [:travel :GQ]]
-    [9 [:valve-on :YA]]
-    [10 [:travel :YA]]
-    [11 [:travel :UJ]]
-    [12 [:valve-on :NS]]
-    [13 [:travel :NS]]
-    [14 [:travel :CV]]
-    [15 [:valve-on :KV]]
-    [16 [:travel :KV]]
-    [17 [:travel :TA]]
-    [18 [:travel :VH]]
-    [19 [:valve-on :DT]]
-    [20 [:travel :DT]]
-    [21 [:travel :HU]]
-    [22 [:travel :TQ]]
-    [23 [:valve-on :SU]]
-    [24 [:travel :SU]]
-    [25 [:travel :OL]]
-    [26 [:valve-on :GG]]
-    [27 [:travel :GG]]
-    [28 [:travel :ML]]
-    [29 [:travel :RI]]
-    [30 [:travel :RJ]]]})
+   [[26 [:valve-on :AA :route-onwards (:KV :PE :RI :RJ :AA)]]
+    [26 [:valve-on :AA :route-onwards (:RI :RJ :AA)]]
+    [24 [:valve-on :RI :route-onwards (:GG :ML :RI)]]
+    [22 [:valve-on :KV :route-onwards (:NS :CV :KV)]]
+    [21 [:valve-on :GG :route-onwards (:SU :OL :GG)]]
+    [19 [:valve-on :NS :route-onwards (:YA :UJ :NS)]]
+    [18 [:valve-on :SU :route-onwards (:JQ :YN :SF :SU)]]
+    [16 [:valve-on :YA :route-onwards (:HR :KO :GQ :YA)]]
+    [14 [:valve-on :JQ :route-onwards (:QF :NJ :JC :JQ)]]
+    [12 [:valve-on :HR :route-onwards (:DT :VH :TA :KV :HN :LR :HR)]]
+    [10 [:valve-on :QF :route-onwards (:UH :YI :QF)]]
+    [7 [:valve-on :UH]]
+    [5 [:valve-on :DT]]]}
+  )

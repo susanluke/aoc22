6401a6321360e3d1b93b719b0dd4d792c5059b46
Susan Luke <sluke000@gmail.com>
2023-01-30 09:05:01 +0000

    Tidy up day 16

--- src/aoc22/day16.clj
+++ src/aoc22/day16.clj
@@ -17,43 +17,10 @@
        (apply concat)
        (apply hash-map)))
 
-(defn max-flow [network-info shortest-paths destination-nodes valve time-left valves-on res-acc]
-  (if (or (>= 0 time-left)
-          (= valves-on destination-nodes))
-    res-acc
-    (->> (conj
-          ;; option 1 - turn on valve and then visit a destination node
-          (for [valve-next (->> (shortest-paths valve)
-                                keys
-                                (remove valves-on))]
-            (let [ ;; path contains start node, which adds one, but we need to
-                  ;; add one anyway to account for turning valve on.
-                  time-used-to-next-node (-> (get-in shortest-paths [valve valve-next])
-                                             count
-                                             dec)
-                  time-used-to-turn-on-valve (if (= :AA valve) 0 1)
-                  time-used (+ time-used-to-next-node time-used-to-turn-on-valve)
-                  flow-rate (get-in network-info [valve :rate])]
-              (max-flow network-info shortest-paths destination-nodes
-                        valve-next
-                        (- time-left time-used)
-                        (conj valves-on valve)
-                        (-> res-acc
-                            (update :pressure-released #(+ % (* flow-rate (dec time-left))))
-                            (update :actions #(conj % [time-left [:valve-on valve]]))))))
-
-          ;; option 2 - turn on valve, but don't do anything else
-          (-> res-acc
-              (update :pressure-released #(+ % (* (get-in network-info [valve :rate]) (dec time-left))))
-              (update :actions #(conj % [time-left [:valve-on valve]]))))
-         (sort-by :pressure-released >)
-         first)))
-
 (defn unvisited-adjoining-nodes [network-info route visited]
   (->> (get-in network-info [(first route) :dests])
        (remove visited)
        (map (fn [next-n] (conj route next-n)))))
-
 
 (defn shortest-path
   "Takes:
@@ -89,23 +56,8 @@
                    (assoc results n route)
                    results)))))))
 
-(defn pt1 [s]
-  (let [network-info      (parse-string s)
-        ;; destination nodes are nodes worth going to because they have a valve
-        ;; which releases pressure
-        destination-nodes (->> network-info
-                               (remove (fn [[k v]] (zero? (:rate v))))
-                               (map first))
-        ;; for each 'destination node', pre-calc the shortest path to each of
-        ;; the other 'destination nodes'.  Then we only need to try these routes.
-        shortest-paths (->> (map #(list % (shortest-path network-info % (set destination-nodes))) (conj destination-nodes :AA))
-                            (apply concat)
-                            (apply hash-map))]
-    (max-flow network-info shortest-paths destination-nodes :AA 30 #{} {:pressure-released 0
-                                                                        :actions           []})))
-
 (defn max-flow2 [network-info shortest-paths destination-nodes time-lefts-and-valves valves-visited valves-on res-acc]
-  (let [[time-left valve] (first time-lefts-and-valves)
+  (let [[time-left valve]          (first time-lefts-and-valves)
         rest-time-lefts-and-valves (rest time-lefts-and-valves)]
     (if (or (nil? time-left)
             (>= 0 time-left)
@@ -113,42 +65,46 @@
       res-acc
       (->> (conj
             ;; option 1 - turn on valve and then visit a destination node
+            ;;
+            ;; TODO - prune these options so that we look at nodes that share
+            ;; part of the path and ignore downstream nodes if it makes sense to
+            ;; turn upstream on.
             (for [valve-next (->> (shortest-paths valve)
                                   keys
                                   (remove valves-visited))]
-              (let [ ;; path contains start node, which adds one, but we need to
-                    ;; add one anyway to account for turning valve on.
-                    route (get-in shortest-paths [valve valve-next])
-                    time-used-to-next-node (-> route
-                                               count
-                                               dec)
+              (let [route                      (get-in shortest-paths
+                                                       [valve valve-next])
+                    time-used-to-next-node     (-> route count dec)
                     time-used-to-turn-on-valve (if (= :AA valve) 0 1)
-                    time-used (+ time-used-to-next-node time-used-to-turn-on-valve)
-                    flow-rate (get-in network-info [valve :rate])]
+                    time-used                  (+ time-used-to-next-node
+                                                  time-used-to-turn-on-valve)
+                    flow-rate                  (get-in network-info
+                                                       [valve :rate])]
                 (max-flow2 network-info shortest-paths destination-nodes
                            (->> (conj rest-time-lefts-and-valves
                                       [(- time-left time-used) valve-next])
                                 (sort-by first >))
-                           (conj valves-visited valve valve-next) ;; need to check we can get to valve-next
-                           (conj valves-on valve) ;; need to check we can get to valve-next
+                           (conj valves-visited valve valve-next)
+                           (conj valves-on valve)
                            (-> res-acc
-                               (update :pressure-released #(+ % (* flow-rate (dec time-left))))
-                               (update :actions #(conj % [time-left [:valve-on valve :route-onwards route]]))))))
+                               (update :pressure-released + (* flow-rate (dec time-left)))
+                               (update :actions conj [time-left {:valve-on      valve
+                                                                 :route-onwards (reverse route)}])))))
 
-            ;; option 2 - turn on valve, but don't do anything else recurse
-            ;; through as partner might be able to do something.  Need to keep
+            ;; option 2 - turn on valve, but don't do anything else. Continue to
+            ;; recurse as partner might be able to do something.  Need to keep
             ;; recursing until we get to the exit conditions.
             (max-flow2 network-info shortest-paths destination-nodes
                        rest-time-lefts-and-valves
                        valves-visited
                        (conj valves-on valve)
                        (-> res-acc
-                           (update :pressure-released #(+ % (* (get-in network-info [valve :rate]) (dec time-left))))
-                           (update :actions #(conj % [time-left [:valve-on valve]])))))
+                           (update :pressure-released + (* (get-in network-info [valve :rate]) (dec time-left)))
+                           (update :actions conj [time-left {:valve-on valve}]))))
            (sort-by :pressure-released >)
            first))))
 
-(defn pt2 [s]
+(defn ptx [s start-nodes time]
   (let [network-info      (parse-string s)
         ;; destination nodes are nodes worth going to because they have a valve
         ;; which releases pressure
@@ -160,28 +116,13 @@
         shortest-paths (->> (map #(list % (shortest-path network-info % (set destination-nodes))) (conj destination-nodes :AA))
                             (apply concat)
                             (apply hash-map))]
-    (max-flow2 network-info shortest-paths destination-nodes [[26 :AA] [26 :AA]] #{} #{} {:pressure-released 0
-                                                                                          :actions           []})))
+    (max-flow2 network-info
+               shortest-paths
+               destination-nodes
+               (mapv #(vector time %) start-nodes)
+               #{} #{}
+               {:pressure-released 0
+                :actions           []})))
 
-(comment
-  "
- 2 The elephant opens valve DD.
- 3 You open valve           JJ.
- 7 You open valve           BB.
- 7 The elephant opens valve HH.
- 9  You open valve          CC.
-11 The elephant opens valve EE.
-"
-
-  ;; no diff between valves visited and on
-  (->> (sut/pt2 input)
-       :actions
-       (map (fn [[t acts]] [(- 27 t) acts])))
-  ([1 [:valve-on :AA :route (:DD :AA)]]
-   [1 [:valve-on :AA :route (:BB :AA)]]
-   [2 [:valve-on :BB :route (:JJ :II :AA :BB)]]
-   [2 [:valve-on :DD :route (:HH :GG :FF :EE :DD)]]
-   [6 [:valve-on :JJ :route (:CC :DD :AA :II :JJ)]]
-   [7 [:valve-on :HH :route (:EE :FF :GG :HH)]]
-   [11 [:valve-on :EE]])
-  )
+(defn pt1 [s] (ptx s [:AA] 30))
+(defn pt2 [s] (ptx s [:AA :AA] 26))

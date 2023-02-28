4076a98b923a3d64eaddcf8dd821dd3107b6f010
Susan Luke <sluke000@gmail.com>
2023-02-28 08:53:14 +0000

    Day 18 Part 2

--- src/aoc22/day18.clj
+++ src/aoc22/day18.clj
@@ -1,61 +1,68 @@
 (ns aoc22.day18
   (:require [clojure.string :as string]))
 
-(defn parse-cube [s]
+(defn parse-lava-droplet [s]
   (->> (string/split s #",")
        (mapv read-string)))
 
 (defn parse-input [s]
   (->> (string/split-lines s)
-       (map parse-cube)))
+       (map parse-lava-droplet)))
 
 (defn neighbours [d]
   (for [f [inc dec]
         i (range (count d))]
-    (update d i f )))
+    (update d i f)))
 
-(defn count-exposed-faces [droplets d]
+(defn neighbours-in-box-not-touching-lava
+  "neighbouring spaces inside box and not occupied by lava.  Used to
+  model how water fills the space."
+  [[[xl xh] [yl yh] [zl zh]] lava d]
   (->> (neighbours d)
-       (remove droplets)
+       (filter (fn [[x y z]]
+                 (and (<= xl x xh)
+                      (<= yl y yh)
+                      (<= zl z zh))))
+       (remove lava)))
+
+(defn count-touching-water-faces [water d]
+  (->> (neighbours d)
+       (filter water)
+       count))
+
+(defn count-exposed-faces [lava d]
+  (->> (neighbours d)
+       (remove lava)
        count))
 
 (defn pt1 [s]
-  (let [droplets (parse-input s)]
-    (->> droplets
-         (map (partial count-exposed-faces (set droplets)))
+(let [lava (parse-input s)]
+  (->> lava
+       (map (partial count-exposed-faces (set lava)))
+       (apply +))))
+
+(defn bounding-box [lava]
+(for [i (range (count (first lava)))]
+  (->> (map #(nth % i) lava)
+       ((fn [ns]
+          [(-> (apply min ns) dec)
+           (-> (apply max ns) inc)])))))
+
+(defn fill-water [box-dims lava visited to-visit w]
+  (let [ns           (->> (neighbours-in-box-not-touching-lava box-dims lava w)
+                          (remove visited))
+        to-visit-new (->> (concat to-visit ns)
+                          (remove visited))
+        next-w       (first to-visit-new)]
+    (if (nil? next-w)
+      visited
+      (recur box-dims lava (conj visited w) (rest to-visit-new) next-w))))
+
+(defn pt2 [s]
+  (let [lava        (parse-input s)
+        box-dims    (bounding-box lava)
+        water-start (mapv first box-dims)
+        water       (fill-water box-dims (set lava) #{} '() water-start)]
+    (->> lava
+         (map (partial count-touching-water-faces water))
          (apply +))))
-
-(comment
-  " Something seems off about your calculation. The cooling rate depends
-on exterior surface area, but your calculation also included the surface area of
-air pockets trapped in the lava droplet.
-
-Instead, consider only cube sides that could be reached by the water and steam
-as the lava droplet tumbles into the pond. The steam will expand to reach as
-much as possible, completely displacing any air on the outside of the lava
-droplet but never expanding diagonally.
-
-In the larger example above, exactly one cube of air is trapped within the lava
-droplet (at 2,2,5), so the exterior surface area of the lava droplet is 58.
-
-What is the exterior surface area of your scanned lava droplet? "
-
-
-  "
-Thoughts
-- Could start at some surface point - eg, lowest x, then lowest y, then lowest z
-- for each
-
-- could create a 'water tank' around min/max x/ys and recursively fill water.
-- whatever isn't water and isn't rock is an air pocket.
-- now count exposed faces of air pocket.
-
-
-- do we think about faces or cubes?
-
-
-
-What if's not a single blob?  There might be multiple blobs?
-
-- check if all the surfaces are accounted for?
---> how would we do this? many cubes would be inside the surface area we are tracking.")
--- test/aoc22/day18_test.clj
+++ test/aoc22/day18_test.clj
@@ -3,7 +3,7 @@
             [clojure.java.io :as io]
             [aoc22.day18 :as sut]))
 
-(def lava-droplets-example
+(def lava-string-example
   "2,2,2
 1,2,2
 3,2,2
@@ -18,10 +18,13 @@
 2,1,5
 2,3,5")
 
-(def lava-droplets
+(def lava-string
   (-> "input18.txt" io/resource slurp))
 
 (deftest pt1
-  (is (= 1 1))
-  (is (= 64 (sut/pt1 lava-droplets-example)))
-  (is (= 4314 (sut/pt1 lava-droplets))))
+  (is (= 64 (sut/pt1 lava-string-example)))
+  (is (= 4314 (sut/pt1 lava-string))))
+
+(deftest pt2
+  (is (= 58 (sut/pt2 lava-string-example)))
+  (is (= 2444 (sut/pt2 lava-string))))
